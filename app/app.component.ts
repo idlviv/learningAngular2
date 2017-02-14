1b27e8117e9cae5eef72ef1ab9e52bb75d4ff20c
idlviv <idlviv@rambler.ru>
2017-02-14 21:49:19 +0200

    home 14/02/17

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,66 @@
 import { Component } from '@angular/core';
 
+// interface ITodo {
+//   title: string;
+//   completed: boolean;
+// }
+
+class Todo {
+  constructor(public title: string,
+              public completed: boolean = false) {}
+
+
+  // title: string;
+  // completed: boolean;
+  //
+  // constructor(title: string, completed: boolean = false) {
+  //   this.title = title;
+  //   this.completed = completed;
+  // }
+}
+
 const listTodo = [
-    'First',
-    'Second',
-    'Third'
+    {
+      title: 'First',
+      completed: false
+    },
+    {
+      title: 'Second',
+      completed: true
+    },
+    {
+      title: 'Third',
+      completed: false
+    },
   ];
 
 @Component({
-    moduleId: module.id,
-    selector: 'app',
-    templateUrl: 'app.component.html',
-    styleUrls: ['app.component.css']
+  moduleId: module.id,
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 
 export class AppComponent {
-    title = 'Hello Angular2';
-    listTodo = listTodo;
+  title: string = 'Hello Angular2';
+  listTodo: Todo[] = listTodo;
+  newTodoTitle: string = '';
+
+  toggle(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  delete(todo: Todo) {
+    let index = this.listTodo.indexOf(todo);
+
+    if (index > -1) {
+      this.listTodo.splice(index,1);
+    }
+  }
+
+  create(event: Event, titleInput: string) {
+    event.preventDefault();
+    let todo: Todo = new Todo (titleInput);
+    this.listTodo.push(todo);
+  }
 }
